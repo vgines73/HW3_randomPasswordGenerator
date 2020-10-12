cf476e83e660ac2dc99081ca6b7a27de88800555
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-12 16:32:29 -0700

    function getMyPasswordOptions for type of length

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getMyPasswordOptions(){
+
+  // get user input of length of password
+  // make sure length is number; not string
+  var length = parseInt(prompt("Length of password?"));
+  console.log(typeof length);
+
+
+
+}
+
+function generatePassword() {
+  getMyPasswordOptions()
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
