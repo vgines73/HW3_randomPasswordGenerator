06a5f39ad34b22442d73b20e3294dd8f7329ab3e
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-13 22:29:11 -0700

    fixed var symbols needed backslash

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "s", "t", "u", "v", "w", "x", "y", "z"];
-var abcCaps = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var symbols = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ']', "^", '_', "`", "{", "|", "}", "~"]
+var abcCaps = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 function getMyPasswordOptions() {
 
@@ -92,10 +92,6 @@ function getMyPasswordOptions() {
 }
 
 
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// "abcdefghijklmnopqrstuvwxyz"
-// "0123456789"
-// " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // 
 
